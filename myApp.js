bd33706686a53ee02506723dbb136cb4360e75a2
kevin-lupera <kevinlupera@gmail.com>
2023-04-04 23:43:58 -0500

    first method get

--- myApp.js
+++ myApp.js
@@ -1,40 +1,11 @@
-let express = require('express');
-console.log('Hello World');
+let express = require("express");
+console.log("Hello World");
 let app = express();
 
+app.get("/", getString);
 
+function getString(req, res) {
+  res.send("Response String");
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
