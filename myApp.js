c843c840d770588f5880a25d5c0ea62cc4db0c5f
kevin-lupera <kevinlupera@gmail.com>
2023-04-05 12:54:00 -0500

    Se agrega bodyParser

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+const bodyParser = require("body-parser");
 let express = require("express");
 console.log("Hello World");
 let app = express();
 
 app.use(middlewareLog);
+app.use(bodyParser.urlencoded({ extended: false }));
 
 function middlewareLog(req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
@@ -11,8 +13,8 @@ function middlewareLog(req, res, next) {
 }
 
 function middlewareTime() {
-  const date =new Date().toString()
-  console.log(date)
+  const date = new Date().toString();
+  console.log(date);
   return date;
 }
 
@@ -40,26 +42,33 @@ function getJson(req, res) {
 }
 
 function getNow(req, res) {
-  res.json({time: req.time})
+  res.json({ time: req.time });
 }
-app.get('/now', function(req, res, next) {
-  req.time = middlewareTime();  // Hypothetical synchronous operation
-  next();
-}, function(req, res) {
-  res.json({time: req.time});
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = middlewareTime(); // Hypothetical synchronous operation
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
+
+app.get("/:word/echo", function (req, res) {
+  res.json({ echo: req.params.word });
 });
 
-app.get('/:word/echo', function(req, res){
-  res.json({echo: req.params.word});
-})
-
-function getName(req, res){
-  res.json({name: `${req.query.first} ${req.query.last}`});
+function getName(req, res) {
+  res.json({ name: `${req.query.first} ${req.query.last}` });
 }
 
-app.route('/name').get(getName).post(getName)
+function sendName(req, res) {
+  res.json({ name: `${req.body.first} ${req.body.last}` });
+}
+
+app.route("/name").get(getName).post(sendName);
 
 app.use("/public", express.static("public"));
-
 
 module.exports = app;
